62a796bc4b29e0cec837f0286814ef9c06664b8f
TMPCDDES <david.ellis@birmingham.gov.uk>
2024-02-16 15:58:29 +0000

    Added small number supression

--- EquiPy/Matrix.py
+++ EquiPy/Matrix.py
@@ -11,15 +11,68 @@ import seaborn as sns
 custom_params = {"axes.spines.right": False, "axes.spines.top": False}
 sns.set_theme(style="ticks", rc=custom_params)
 
+
+def small_number_supression(
+        data,
+        column, 
+        value_pivot,
+        eth_col = "Ethnicity", 
+        IMD_col = "IMD",
+        supp_thresh = 5):
+    '''
+    Parameters
+    ----------
+    data : DataFrame
+        Source data containing ethnicity and deprivation columns
+    value_pivot : DataFrame
+        pivot table of counts accross
+    supp_thresh : int, optional
+        Values relating to counts below this value will be supressed.
+        The default is 5.
+    ttest: bool
+        
+    Returns
+    -------
+    values : numpy array
+        Array of values that are allowed following supression
+    labels : numpy array
+        Array of labels.
+    '''
+    
+    # Count number of cases associated with each value
+    count_pivot = data.pivot_table(values = column, index = IMD_col, 
+                                   columns = eth_col, aggfunc = "count")
+    counts = count_pivot.values
+
+    # Get pivot values and dimentions
+    vals = value_pivot.values
+    imax, jmax = vals.shape
+    
+    # Prepare empty labels matrix
+    labels = np.zeros((imax, jmax), dtype=object)
+    
+    for i in range(imax):
+        for j in range(jmax):
+            val_ij = vals[i, j]
+            count_ij = counts[i, j]
+            
+            if (count_ij<supp_thresh)*(count_ij>0):
+                labels[i,j] = "Too\nsmall"
+                vals[i, j] = 0
+            else:
+                labels[i,j] = str(np.round(val_ij,1))
+
+    return vals, labels
+
 # define inequality matrix function
 def inequality_map(data, 
                    column, 
                    palette = "Purples",
-                   eth_col = "Ethnicity Group", 
-                   IMD_col = "IMD Quintile - ALL",
+                   eth_col = "Ethnicity", 
+                   IMD_col = "IMD",
                    agg="mean",
-                   fmt=".1f",
-                   letter = ""):
+                   letter = "",
+                   supp_thresh = 5):
     
     # Get bar color that matches the chosen palette
     bar_col = plt.colormaps[palette](0.8)
@@ -35,18 +88,27 @@ def inequality_map(data,
         multiply = 1
         
     data[agg_col] = multiply*data[column]
-
     eth_imd_piv = data.pivot_table(values = column, index = IMD_col, 
                                        columns = eth_col, aggfunc= agg)
+    
+    # Apply small number supression
+    vals, labels = small_number_supression(
+            data,
+            column, 
+            multiply*eth_imd_piv,
+            eth_col = eth_col, 
+            IMD_col = IMD_col,
+            supp_thresh = supp_thresh)
 
     fig = plt.figure(figsize=(8, 8))
     gs = fig.add_gridspec(8, 8)
 
     ax1 = fig.add_subplot(gs[2:8, :6])
-    sns.heatmap(multiply*eth_imd_piv, annot=True, fmt = fmt, linewidths=.5, 
-                ax=ax1, cmap = palette, cbar=False)
+    sns.heatmap(vals, annot=labels, fmt="",
+                linewidths=.5, ax=ax1, cmap = palette, cbar=False)
 
     ax1.set_yticklabels(ax1.get_yticks(), rotation = 0)
+    
     # TODO: Develop dynamic solution for this labelling
     ax1.set_yticklabels(["1\nMost\ndeprived","2","3","4","5\nLeast\ndeprived"])
     
