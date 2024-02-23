66b764092662ce958e78246e44a347d9eec95834
TMPCDDES <david.ellis@birmingham.gov.uk>
2024-02-23 10:24:18 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='inequality-vis', 
+    name='EquiPy', 
     version='0.1',  
     author='David Ellis',
     author_email='david.ellis@birmingham.gov.uk',
     description='Library to visualise differences in rates and outcomes across ethnicity and index of multiple deprivation',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/David-Ellis/inequality-vis',
+    url='https://github.com/BCC-PHM/EquiPy',
     packages=find_packages(),
     install_requires=[  
         'pandas',
